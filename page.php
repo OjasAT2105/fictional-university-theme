3dcd290f39e89bbad75b817889f15b4719036d58
OjasAT2105 <your_email@example.com>
2025-09-07 22:22:22 +0530

    Add initial theme files and structure for Fictional University
    - Created main theme files including header, footer, and front-page templates.
    - Added custom post types for events, programs, and professors.
    - Implemented site header styles and responsive menu functionality.
    - Integrated hero slider and Google Maps functionality.
    - Enqueued necessary scripts and styles in functions.php.
    - Established basic styling and layout for the theme.
    - Included images for the hero section and other components.

---
+++ page.php
@@ -0,0 +1,56 @@
+<?php 
+    get_header();
+    
+    while(have_posts()){
+        the_post(); ?>
+         <div class="page-banner">
+            <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
+            <div class="page-banner__content container container--narrow">
+                <h1 class="page-banner__title"><?php the_title(); ?></h1>
+                <div class="page-banner__intro">
+                <p>DONT FORGET TO REPLACE ME LATER  </p>
+                </div>
+            </div>
+         </div>
+
+    <div class="container container--narrow page-section">
+        <?php
+        $the_parent = wp_get_post_parent_id(get_the_ID()); // on the child page this value is always greater than 0 and if the page is a parent page then the value will always be 0
+        if( wp_get_post_parent_id(get_the_ID())){?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p>
+                <a class="metabox__blog-home-link" href="<?php echo get_permalink($the_parent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($the_parent); ?></a> <span class="metabox__main"><?php the_title(); ?></span>
+                </p>
+            </div>
+        <?php }?>
+
+
+        <?php $testArray = get_pages(array(
+            'child_of' => get_the_ID()
+        ));?> 
+        <?php if($the_parent || $testArray ){?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_permalink($the_parent); ?>"><?php echo get_the_title($the_parent); ?></a></h2>
+        <ul class="min-list">
+          <?php 
+          $find_children_of = $the_parent ? $the_parent : get_the_ID();
+          wp_list_pages(array(
+            'title_li' => null,
+            'child_of' => $find_children_of,
+            'sort_column' => 'menu_order',
+            'sort_order' => 'ASC'
+          ));
+          ?>
+        </ul>
+      </div>
+      <?php }?>
+
+
+
+      <div class="generic-content">
+        <p><?php the_content(); ?></p>
+      </div>
+    </div>
+    <?php }
+?>
+<?php get_footer(); ?>
