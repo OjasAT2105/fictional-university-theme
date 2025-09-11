8e98ed7321bb2057c1f545ecbe15309f08acd5ce
OjasAT2105 <your_email@example.com>
2025-09-11 16:52:04 +0530

    Added a create like functionality for professor

--- src/modules/Like.js
+++ src/modules/Like.js
@@ -11,18 +11,45 @@ class Like {
   ourClickDispatcher(e) {
     var currentLikeBox = $(e.target).closest(".like-box"); // this code for selection of like box of user clicked, generally this case is for scenerio in which if the same page has multiple like boxes thats why we are using event to check its closest ones.
     if (currentLikeBox.data("exists") == "yes") {
-      this.deleteLike();
+      this.deleteLike(currentLikeBox);
     } else {
-      this.createLike();
+      this.createLike(currentLikeBox);
     }
   }
 
-  createLike() {
-    alert("create");
+  createLike(currentLikeBox) {
+    $.ajax({
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
+      },
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      type: "POST",
+      data: {
+        professorId: currentLikeBox.data("professor"),
+      },
+      success: (response) => {
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
   }
 
   deleteLike() {
-    alert("delete");
+    $.ajax({
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      type: "DELETE",
+      data: {
+        professorId: 6,
+      },
+      success: (response) => {
+        console.log(response);
+      },
+      error: (response) => {
+        console.log(response);
+      },
+    });
   }
 }
 export default Like;
