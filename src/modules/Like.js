82a05bd4fabcd9888997b89073f932161d84bc83
OjasAT2105 <your_email@example.com>
2025-09-11 17:26:31 +0530

    Completed the delete like functionality for professor posts

--- src/modules/Like.js
+++ src/modules/Like.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+
 class Like {
   constructor() {
     this.events();
@@ -8,9 +9,11 @@ class Like {
     $(".like-box").on("click", this.ourClickDispatcher.bind(this));
   }
 
+  // methods
   ourClickDispatcher(e) {
-    var currentLikeBox = $(e.target).closest(".like-box"); // this code for selection of like box of user clicked, generally this case is for scenerio in which if the same page has multiple like boxes thats why we are using event to check its closest ones.
-    if (currentLikeBox.data("exists") == "yes") {
+    var currentLikeBox = $(e.target).closest(".like-box");
+
+    if (currentLikeBox.attr("data-exists") == "yes") {
       this.deleteLike(currentLikeBox);
     } else {
       this.createLike(currentLikeBox);
@@ -24,10 +27,13 @@ class Like {
       },
       url: universityData.root_url + "/wp-json/university/v1/manageLike",
       type: "POST",
-      data: {
-        professorId: currentLikeBox.data("professor"),
-      },
+      data: { professorId: currentLikeBox.data("professor") },
       success: (response) => {
+        currentLikeBox.attr("data-exists", "yes");
+        var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+        likeCount++;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", response);
         console.log(response);
       },
       error: (response) => {
@@ -36,14 +42,20 @@ class Like {
     });
   }
 
-  deleteLike() {
+  deleteLike(currentLikeBox) {
     $.ajax({
-      url: universityData.root_url + "/wp-json/university/v1/manageLike",
-      type: "DELETE",
-      data: {
-        professorId: 6,
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("X-WP-Nonce", universityData.nonce);
       },
+      url: universityData.root_url + "/wp-json/university/v1/manageLike",
+      data: { like: currentLikeBox.attr("data-like") },
+      type: "DELETE",
       success: (response) => {
+        currentLikeBox.attr("data-exists", "no");
+        var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+        likeCount--;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", "");
         console.log(response);
       },
       error: (response) => {
@@ -52,4 +64,5 @@ class Like {
     });
   }
 }
+
 export default Like;
